9d019f1d9c77d58bce79e225c4494f8edd97ece3
HxHexa <tquang1708@gmail.com>
2020-07-31 16:49:00 -0400

    cleaned up a bit in opcode matching

--- 2019/quang/day5/src/main.rs
+++ 2019/quang/day5/src/main.rs
@@ -24,10 +24,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .expect("cpu input parsring fail");
 
     //virtual cpu parsing logic
-    let mut curr_pos:usize = 0;
+    let mut curr_pos: usize = 0;
     loop {
-        println!("{:?}", int_input);
-
         if curr_pos > int_input.len() - 1 {
             panic!("vector index overflow at {}", curr_pos);
         }
@@ -42,37 +40,33 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         //parsing param and storing appropriate param val
         let param_mode = (instruction - opcode) / 100;
-        let mut param_vec:Vec<u8> = param_mode.digits().rev().collect();
+        let mut param_vec: Vec<u8> = param_mode.digits().rev().collect();
         while param_vec.len() < 3 {
             param_vec.push(0);
         }
         let mut param_vec_iter = param_vec.iter();
-        println!("{:?}", param_vec);
 
         match opcode {
-            1 => {
+            1|2 => {
                 let mut newnum = 0;
-                for i in param_vec_iter.take(2) {
-                    curr_pos += 1;
-                    newnum += match i {
-                        0 => int_input[int_input[curr_pos] as usize],
-                        1 => int_input[curr_pos],
-                        _ => panic!("Invalid param mode"),
+                if opcode == 1{
+                    for i in param_vec_iter.take(2) {
+                        curr_pos += 1;
+                        newnum += match i {
+                            0 => int_input[int_input[curr_pos] as usize],
+                            1 => int_input[curr_pos],
+                            _ => panic!("Invalid param mode"),
+                        }
                     }
-                }
-                curr_pos += 1;
-                let mod_index = int_input[curr_pos] as usize;
-                int_input[mod_index] = newnum;
-                curr_pos += 1;
-            }
-            2 => {
-                let mut newnum = 1;
-                for i in param_vec_iter.take(2) {
-                    curr_pos += 1;
-                    newnum *= match i {
-                        0 => int_input[int_input[curr_pos] as usize],
-                        1 => int_input[curr_pos],
-                        _ => panic!("Invalid param mode"),
+                } else {
+                    newnum += 1;
+                    for i in param_vec_iter.take(2) {
+                        curr_pos += 1;
+                        newnum *= match i {
+                            0 => int_input[int_input[curr_pos] as usize],
+                            1 => int_input[curr_pos],
+                            _ => panic!("Invalid param mode"),
+                        }
                     }
                 }
                 curr_pos += 1;
@@ -91,37 +85,24 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 println!("OP4 output: {}", int_input[int_input[curr_pos] as usize]);
                 curr_pos += 1;
             }
-            5 => {
+            5 | 6 => {
                 curr_pos += 1;
                 let check_val = match param_vec_iter.next().unwrap() {
                     0 => int_input[int_input[curr_pos] as usize],
                     1 => int_input[curr_pos],
                     _ => panic!("Invalid param mode"),
                 };
-                if check_val != 0 {
+                if ((check_val != 0) & (opcode == 5)) | ((check_val == 0) & (opcode == 6)) {
                     curr_pos = match param_vec_iter.next().unwrap() {
                         0 => int_input[int_input[curr_pos + 1] as usize] as usize,
                         1 => int_input[curr_pos + 1] as usize,
                         _ => panic!("Invalid param mode"),
                     };
-                } else { curr_pos += 2; }
+                } else {
+                    curr_pos += 2;
+                }
             }
-            6 => {
-                curr_pos += 1;
-                let check_val = match param_vec_iter.next().unwrap() {
-                    0 => int_input[int_input[curr_pos] as usize],
-                    1 => int_input[curr_pos],
-                    _ => panic!("Invalid param mode"),
-                };
-                if check_val == 0 {
-                    curr_pos = match param_vec_iter.next().unwrap() {
-                        0 => int_input[int_input[curr_pos + 1] as usize] as usize,
-                        1 => int_input[curr_pos + 1] as usize,
-                        _ => panic!("Invalid param mode"),
-                    };
-                } else { curr_pos += 2; }
-            }
-            7 => {
+            7 | 8 => {
                 let mut comp_vec = Vec::new();
                 for i in param_vec_iter.take(2) {
                     curr_pos += 1;
@@ -133,26 +114,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 }
                 curr_pos += 1;
                 let mod_index = int_input[curr_pos] as usize;
-                if comp_vec[0] < comp_vec[1] {
+                if ((comp_vec[0] < comp_vec[1]) & (opcode == 7))
+                    | ((comp_vec[0] == comp_vec[1]) & (opcode == 8))
+                {
                     int_input[mod_index] = 1;
-                } else { int_input[mod_index] = 0; }
-                curr_pos += 1;
-            }
-            8 => {
-                let mut comp_vec = Vec::new();
-                for i in param_vec_iter.take(2) {
-                    curr_pos += 1;
-                    comp_vec.push(match i {
-                        0 => int_input[int_input[curr_pos] as usize],
-                        1 => int_input[curr_pos],
-                        _ => panic!("Invalid param mode"),
-                    })
+                } else {
+                    int_input[mod_index] = 0;
                 }
-                curr_pos += 1;
-                let mod_index = int_input[curr_pos] as usize;
-                if comp_vec[0] == comp_vec[1] {
-                    int_input[mod_index] = 1;
-                } else { int_input[mod_index] = 0; }
                 curr_pos += 1;
             }
             _ => panic!("invalid input {}", int_input[curr_pos]),
